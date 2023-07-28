4b2267c9e0b117fc4869b763f7e01bbaa158d5fe
Carlos <carlosariza7459@gmail.com>
2023-07-28 16:11:35 -0400

    Add project files.

---
+++ TallerUSS/TallerUSS/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TallerUSS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearCuenta_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCorreo.Text))
+            {
+                DisplayAlert("Error", "El correo no puede ser vacio.", "OK");
+            }
+            else if (picker.SelectedIndex == -1)
+            {
+                DisplayAlert("Error", "Seleccione año ingreso.", "OK");
+            }
+            else if (optFemenino.IsChecked == false && optMasculino.IsChecked == false)
+            {
+                DisplayAlert("Error", "Seleccione genero.", "OK");
+
+            }
+            else if (checkTerminos.IsChecked == false)
+            {
+                DisplayAlert("Error", "Para crear cuenta debe aceptar terminos.", "OK");
+            }
+            else
+            {
+                DisplayAlert("ÉXITO", "SU CUENTA HA SIDO CREADA", "OK");
+                Navigation.PushAsync(new PaginaExito());
+            }
+        }
+    }
+}
---
+++ TallerUSS/TallerUSS/PaginaExito.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerUSS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PaginaExito : ContentPage
+    {
+        public PaginaExito()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtras_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+    }
+}
